d8adfceac1bc19d3b9bcb51414465a8735ef3ef6
DANNJUGUNA <ndan30071@gmail.com>
2023-05-25 10:38:19 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :mpays
+  resources :mpasas
+
+  post 'stkpush', to: 'mpasas#stkpush'
+  post 'polling_payment', to: 'mpasas#polling_payment'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
